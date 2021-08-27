7071008abdb68ab6f209b17f2df7a4e4debf4c9d
zhangjiahui05 <zhangjiahui05@zhangjiahui05.local>
2021-08-27 18:11:10 +0800

    uuid 涨知识了！

---
+++ 那些我不知道的小知识/uuid.ts
@@ -0,0 +1,8 @@
+const uuidv4 = require('uuid/v4');
+console.log(uuidv4())//29fc5bed-fb2d-4c90-8b03-2864928a536b 类似于这种
+
+/**
+ * uuid---universally unique identifier 通用唯一识别码
+ * 是一组32位数的16进制数字所构成
+ * xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
+ */
